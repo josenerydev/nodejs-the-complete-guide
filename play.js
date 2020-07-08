3d5e4655d317569143abec0b9b86a79e33cce9d4
José Nery <josenerydev@gmail.com>
2020-07-08 18:12:20 -0300

    20. Async Code & Promises

--- play.js
+++ play.js
@@ -1,20 +1,23 @@
-const person = {
-  name: 'Max',
-  age: 29,
-  greet() {
-    console.log('Hi, I am ' + this.name);
-  }
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, 1500);
+  });
+  return promise;
 };
 
-const printName = ({ name }) => {
-  console.log(name);
-}
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData()
+    .then(text => {
+      console.log(text);
+      return fetchData();
+    })
+    .then(text2 => {
+      console.log(text2);
+    });
+}, 2000);
 
-printName(person);
-
-const { name, age } = person;
-console.log(name, age);
-
-const hobbies = ['Sports', 'Cooking'];
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1, hobby2);
+console.log('Hello!');
+console.log('Hi!');
