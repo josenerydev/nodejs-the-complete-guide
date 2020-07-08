025f9d22fe82d8ba472dded45539cd8f2678b7d0
José Nery <josenerydev@gmail.com>
2020-07-08 17:02:19 -0300

    14. Understanding Arrow Functions

--- play.js
+++ play.js
@@ -4,8 +4,17 @@ const hasHobbies = true;
 
 age = 30;
 
-function summarizeUser(userName, userAge, userHasHobby) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
   return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby;
 }
+
+// const add = (a, b) => a + b;
+// console.log(add(1, 2));
+
+// const addOne = a => a + 1;
+// console.log(addOne(1));
+
+const addRandom = () => 1 + 2;
+console.log(addRandom());
 
 console.log(summarizeUser(name, age, hasHobbies));
