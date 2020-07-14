71034ebfed6ee854667cd0f1cc6cdf91508e656e
José Nery <josenerydev@gmail.com>
2020-07-14 18:43:33 -0300

    128. Deleting Cart Items

--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const rootDir = require('../utils/path');
+const Cart = require('./cart');
 
 const p = path.join(rootDir, 'data', 'products.json');
 
@@ -45,10 +46,11 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
       const updatedProducts = products.filter(p => p.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProducts), err => {
         if (!err) {
-
+          Cart.deleteProduct(id, product.price);
         }
       });
     });
