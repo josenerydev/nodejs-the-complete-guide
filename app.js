fe0ee247424437c8897fbf99d3be71c4888839b9
José Nery <josenerydev@gmail.com>
2020-08-03 15:46:58 -0300

    182. Using the Database Connection

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./utils/database");
+const mongoConnect = require("./utils/database").mongoConnect;
 
 const app = express();
 
