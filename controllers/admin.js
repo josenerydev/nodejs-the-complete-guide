407b7ed3ec1c0d30b65ffe4e7deca33d92f8e020
José Nery <josenerydev@gmail.com>
2020-08-03 17:22:18 -0300

    189. Finishing the 'Update Product' Code

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require("mongodb");
 const Product = require("../models/product");
+
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -48,14 +51,16 @@ exports.getEditProduct = (req, res, next) => {
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const { title, price, imageUrl, description } = req.body;
-  Product.findById(prodId)
-    .then((product) => {
-      product.title = title;
-      product.price = price;
-      product.imageUrl = imageUrl;
-      product.description = description;
-      return product.save();
-    })
+
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    new ObjectId(prodId)
+  );
+  product
+    .save()
     .then((result) => {
       console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
