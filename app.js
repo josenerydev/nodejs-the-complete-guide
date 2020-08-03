6db34432caca0a9b730896a3b50adadf16c0e7a6
José Nery <josenerydev@gmail.com>
2020-08-03 18:20:59 -0300

    193. Creating New Users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./utils/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -16,6 +17,16 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("5f287f73103663374859dff6")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+  next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
