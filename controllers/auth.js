96e6872815b8bdf40c3700f0699a30613b6799b0
José Nery <josenerydev@gmail.com>
2020-08-04 10:12:55 -0300

    233. Adding the Request Driven Login Solution

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,5 +2,11 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: req.isLoggedId,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  req.isLoggedId = true;
+  res.redirect("/");
 };
