57ad120e6d1ee05ac636c160249155f774ad30ff
José Nery <josenerydev@gmail.com>
2020-08-04 21:40:22 -0300

    272. Using Nodemailer to Send an Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -86,7 +97,14 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
-        });
+          return transporter.sendMail({
+            to: email,
+            from: "josenerydev@gmail.com",
+            subject: "Signup succeeded!",
+            html: "<h1>You successfully signed up!",
+          });
+        })
+        .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
 };
