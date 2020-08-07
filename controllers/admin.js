38a620337f4ad9cd2d0a028ae325c995aa08a938
José Nery <josenerydev@gmail.com>
2020-08-07 17:24:24 -0300

    313. Status Codes

--- controllers/admin.js
+++ controllers/admin.js
@@ -45,7 +45,6 @@ exports.postAddProduct = (req, res, next) => {
   product
     .save()
     .then((result) => {
-      console.log("Created Product");
       res.redirect("/admin/products");
     })
     .catch((err) => {
