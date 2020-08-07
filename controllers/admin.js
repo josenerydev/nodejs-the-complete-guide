40f645983db3748a5b7a085a571b615dabd646f6
José Nery <josenerydev@gmail.com>
2020-08-07 17:03:50 -0300

    310. Using the Express.js Error Handling Middleware

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,7 +65,10 @@ exports.postAddProduct = (req, res, next) => {
       //   errorMessage: "Database operation failed, please try again.",
       //   validationErrors: [],
       // });
-      res.redirect("/500");
+      // res.redirect("/500");
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -93,7 +96,9 @@ exports.getEditProduct = (req, res, next) => {
       });
     })
     .catch((err) => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
