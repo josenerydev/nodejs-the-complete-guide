b167def521dc1dfa3e95b4dfb7b553ab0efb5e82
José Nery <josenerydev@gmail.com>
2020-08-08 12:36:02 -0300

    365- Sending POST Requests

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,6 +7,7 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  console.log(title, content);
 
   res.status(201).json({
     message: "Post created successfully!",
