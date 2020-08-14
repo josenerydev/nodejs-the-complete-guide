dd344b1312fcbfe08e68b68051b94e440ba4e38f
José Nery <josenerydev@gmail.com>
2020-08-14 10:54:21 -0300

    468. What Not To Test

--- backend/test/auth-middleware.js
+++ backend/test/auth-middleware.js
@@ -22,4 +22,23 @@ describe("Auth middleware", function () {
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
   });
+
+  it("should throw an error if the token cannot be verified.", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId after decoding the token.", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer asfdabvbvg";
+      },
+    };
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+  });
 });
