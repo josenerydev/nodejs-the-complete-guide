97a77d4732f09474b5948cb2d1e20a3d6b2e774a
José Nery <josenerydev@gmail.com>
2020-08-14 15:31:37 -0300

    474. Cleaning Up

--- backend/test/auth-controller.js
+++ backend/test/auth-controller.js
@@ -54,7 +54,13 @@ describe("Auth Controller - Login", function () {
         AuthController.getUserStatus(req, res, () => {}).then(() => {
           expect(res.statusCode).to.be.equal(200);
           expect(res.userStatus).to.be.equal("I am new!");
-          done();
+          User.deleteMany({})
+            .then(() => {
+              return mongoose.disconnect();
+            })
+            .then(() => {
+              done();
+            });
         });
       })
       .catch((err) => console.log(err));
